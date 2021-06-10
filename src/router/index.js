8362b27e36dc6ec740d5b834d87d70974d3d2779
ianbelfatto <ibelfatto@gmail.com>
2021-06-10 17:30:22 -0400

    adds signup, login, and logout features with redirects

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,40 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Users/Signup.vue";
+import Login from "../views/Users/Login.vue";
+import Logout from "../views/Users/Logout.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "Home",
     component: Home,
   },
   {
-    path: "/about",
+    path: "/",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
   },
 ];
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      This website lists out some movies, and allows users to create their own listings!
+      <br />
+      Click 'Home' to see the movie listings, or create your own!
+      <br />
+      OR - Click 'Signup' to create an account.
+    </h1>
   </div>
 </template>
