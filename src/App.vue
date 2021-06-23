5093c6e4a57454d376f7709b3bd4fac7dfc24628
ianbelfatto <ibelfatto@gmail.com>
2021-06-23 15:15:11 -0400

    adds new navbar to app vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">About</router-link>
-      |
-      <router-link to="/movies">Movies (Index)</router-link>
-      |
-      <router-link to="/movies/new">New Movies</router-link>
-      |
-      <span v-if="isLoggedIn()">
-        <router-link to="/logout">Logout</router-link>
-      </span>
-      <span v-else>
-        <router-link to="/signup">Signup</router-link>
-        |
-        <router-link to="/login">Login</router-link>
-        |
-      </span>
-    </div>
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/movies">Movies</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/movies/new">Add Movie</router-link>
+            </li>
+            <span v-if="isLoggedIn()">
+              <li class="nav-item">
+                <router-link class="nav-link active" to="/logout">Logout</router-link>
+              </li>
+            </span>
+            <span v-else>
+              <li class="nav-item">
+                <router-link class="nav-link active" to="/signup">Signup</router-link>
+              </li>
+
+              <li class="nav-item">
+                <router-link class="nav-link active" to="/login">Login</router-link>
+              </li>
+            </span>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
